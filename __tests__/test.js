c22dd0d902109cc2badc88ec8e739e1a7149c4ec
mahendar <mahendarchikkolla@gmail.com>
2023-06-02 11:37:18 +0530

    added signup signin test

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,67 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+const request = require("supertest");
+var cheerio = require("cheerio");
+const db = require("../models/index");
+const app = require("../app");
+
 describe("First test suite ", () => {
-    test("First case", () => {
-        expect(true).toBe(true);
-    })
-})
+  test("First case", () => {
+    expect(true).toBe(true);
+  });
+});
+
+let server, agent;
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name = _csrf]").val();
+}
+
+const login = async (agent, username, password) => {
+  let res = await agent.get("/login");
+  let csrfToken = extractCsrfToken(res);
+  res = await agent.post("/signin").send({
+    email: username,
+    password: password,
+    _csrf: csrfToken,
+  });
+};
+
+describe("Sports Application", function () {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(4000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  test("Sign up", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/users").send({
+      firstName: "Test",
+      lastName: "User A",
+      email: "user.a@test.com",
+      password: "12345678",
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("Sign out", async () => {
+    let res = await agent.get("/sport");
+    expect(res.statusCode).toBe(200);
+    res = await agent.get("/signout");
+    expect(res.statusCode).toBe(302);
+    res = await agent.get("/sport");
+    expect(res.statusCode).toBe(302);
+  });
+});
